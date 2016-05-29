27a48c7665c8bdb575f65dcc96d5b3a03e24b844
TukanDash <tukandash@outlook.es>
2016-05-29 19:13:38 +0200

    Se define base de datos: Creacion de tablas y definicion de campos

--- mproject.sql
+++ mproject.sql
@@ -1,3 +1,4 @@
+# Nombre del fichero: mproject.sql
 
 --
 -- Base de datos: `Mproject`
@@ -5,8 +6,22 @@
 
 -- --------------------------------------------------------
 
+
+
+# Borramos la base de datos Mproject si existe
+DROP DATABASE IF EXISTS Mproject;
+
+# Creamos la base de datos Mproject si no existe.
+CREATE DATABASE IF NOT EXISTS Mproject;
+
+# Abrimos la base de datos Mproject
+USE Mproject;
+
+# Borramos la tabla proyecto si existe
+DROP TABLE IF EXISTS proyecto;
+
 --
--- Estructura de tabla para la tabla `participante`
+-- Estructura de tabla para la tabla `proyecto`
 --
 
 CREATE TABLE IF NOT EXISTS `proyecto` (
@@ -14,17 +29,18 @@ CREATE TABLE IF NOT EXISTS `proyecto` (
   `nombre` varchar(85) NOT NULL,
   `descripcion` text,
   `porcentaje` decimal(6,2) DEFAULT 0.00,
-  `closed` boolean DEFAULT 0,
-  --`num_proyectos` int(11) DEFAULT 0,
+  `estado` ENUM('abierto','ejecucion','espera','finalizado') NOT NULL DEFAULT 'abierto',
+  `archived` boolean DEFAULT 0,
+  `es_proceso` boolean DEFAULT 0,
   PRIMARY KEY (`id_proyecto`),
-  KEY `i_nombre` (`nombre`)
+  KEY `i_nombre_pro` (`nombre`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1001 ;
 
 --
--- Volcado de datos para la tabla `participante`
+-- Volcado de datos para la tabla `proyecto`
 --
 
-INSERT INTO `proyecto` (`id_proyecto`, `nombre`, `descripcion`) VALUES
+INSERT INTO `proyecto` (`id_proyecto`, `nombre`) VALUES
 (1001, 'Proyecto01');
 (1002, 'Proyecto02');
 (1003, 'Proyecto03');
@@ -34,64 +50,78 @@ INSERT INTO `proyecto` (`id_proyecto`, `nombre`, `descripcion`) VALUES
 
 -- --------------------------------------------------------
 
+# Borramos la tabla actividad si existe
+DROP TABLE IF EXISTS actividad;
+
 --
--- Estructura de tabla para la tabla `regalo`
+-- Estructura de tabla para la tabla `actividad`
 --
 
-CREATE TABLE IF NOT EXISTS `regalo` (
-  `id_regalo` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(55) NOT NULL,
-  `beneficiario` int(11) NOT NULL,
-  `valor` decimal(8,2) NOT NULL,
-  `ffijacion` DATETIME DEFAULT CURRENT_TIMESTAMP,
-  `fconsolidacion` date DEFAULT NULL,
-  `total_logrado` decimal (8.2) DEFAULT 0,
-  `costo_paquete` decimal(8,2) DEFAULT 0,
-  `plazo` enum('INMINENTE','CORTO PLAZO','MEDIO PLAZO','LARGO PLAZO') DEFAULT 'MEDIO PLAZO',
-  `descripcion` text DEFAULT NULL,
-  PRIMARY KEY (`id_regalo`),
-  FOREIGN KEY (`beneficiario`)
-      REFERENCES participante(`id_participante`)
-      ON UPDATE CASCADE ON DELETE RESTRICT,
-  KEY `i_titulo` (`titulo`)
+CREATE TABLE IF NOT EXISTS `actividad` (
+  `id_actividad` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(85) NOT NULL,
+  `descripcion` text,
+  `porcentaje` decimal(6,2) DEFAULT 0.00,
+  `es_proceso` boolean DEFAULT 0,
+  `id_proyect` int(11),
+  PRIMARY KEY (`id_actividad`),
+  FOREIGN KEY (`id_proyect`) REFERENCES proyecto(id_proyecto) ON UPDATE CASCADE ON DELETE CASCADE,
+  KEY `i_nombre_act` (`nombre`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
 
 --
--- Volcado de datos para la tabla `regalo`
+-- Volcado de datos para la tabla `actividad`
 --
 
-INSERT INTO `regalo` (`id_regalo`, `titulo`, `beneficiario`, `valor`) VALUES
-(10001, 'Wacom Cintiq HD13', 1001, 850),
-(10002, 'MacBook Pro', 1001, 1200);
+INSERT INTO `actividad` (`id_actividad`, `nombre`, `id_proyect`) VALUES
+(10001, 'Actividad01', 1001);
+(10002, 'Actividad02', 1002);
+(10003, 'Actividad03', 1002);
+(10004, 'Actividad04', 1003);
+(10005, 'Actividad05', 1004);
+(10006, 'Actividad06', 1004);
+
 
 -- --------------------------------------------------------
 
+# Borramos la tabla tarea si existe
+DROP TABLE IF EXISTS tarea;
+
 --
--- Estructura de tabla para la tabla `donacion`
+-- Estructura de tabla para la tabla `tarea`
 --
 
-CREATE TABLE IF NOT EXISTS `donacion` (
-  `id_donacion` int(11) NOT NULL AUTO_INCREMENT,
-  `donante` int(11) NOT NULL,
-  `regalo` int(11) NOT NULL,
-  `cantidad` decimal(8,2) NOT NULL,
-  `fdonacion` DATETIME DEFAULT CURRENT_TIMESTAMP,
-  `motivo` text DEFAULT NULL,
-  PRIMARY KEY (`id_donacion`),
-  FOREIGN KEY (`donante`)
-      REFERENCES participante(`id_participante`)
-      ON UPDATE CASCADE ON DELETE RESTRICT,
-  FOREIGN KEY (`regalo`)
-      REFERENCES regalo(`id_regalo`)
-      ON UPDATE CASCADE ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS `tarea` (
+  `id_tarea` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(85) NOT NULL,
+  `descripcion` text,
+  `done` boolean DEFAULT 0,
+  `id_act` int(11),
+  PRIMARY KEY (`id_tarea`),
+  FOREIGN KEY (`id_act`) REFERENCES actividad(id_actividad) ON UPDATE CASCADE ON DELETE CASCADE,
+  KEY `i_nombre_tar` (`nombre`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=100001 ;
 
 --
--- Volcado de datos para la tabla `donacion`
+-- Volcado de datos para la tabla `tarea`
 --
 
-INSERT INTO `donacion` (`id_donacion`, `donante`, `regalo`, `cantidad`) VALUES
-(100001, 1001, 10001, 50),
-(100002, 1002, 10001, 100),
-(100003, 1002, 10002, 200),
-(100004, 1005, 10001, 50);
+INSERT INTO `actividad` (`id_actividad`, `nombre`, `id_proyect`) VALUES
+(100001, 'Tarea01', 10001);
+(100002, 'Tarea02', 10002);
+(100003, 'Tarea03', 10002);
+(100004, 'Tarea04', 10003);
+(100005, 'Tarea05', 10004);
+(100006, 'Tarea06', 10004);
+(100007, 'Tarea07', 10001);
+(100008, 'Tarea08', 10005);
+(100009, 'Tarea09', 10005);
+(100010, 'Tarea10', 10005);
+(100011, 'Tarea11', 10005);
+(100012, 'Tarea12', 10006);
+(100013, 'Tarea13', 10006);
+(100014, 'Tarea14', 10003);
+(100015, 'Tarea15', 10002);
+(100016, 'Tarea16', 10003);
+(100017, 'Tarea17', 10004);
+(100018, 'Tarea18', 10004);
